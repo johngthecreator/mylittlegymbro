da4d8a6c96d752cf4da61202f0dd2957d3faf219
John Gorriceta <johngorriceta1@gmail.com>
2025-08-30 19:41:48 -0600

    all items

--- app/(tabs)/scan/_layout.tsx
+++ app/(tabs)/scan/_layout.tsx
@@ -1,12 +1,27 @@
-import { Stack } from "expo-router";
+import { Link, Stack } from "expo-router";
+import { Pressable } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
 export default function ScanLayout() {
   return (
     <Stack>
       <Stack.Screen
         name="index"
-        options={{ headerShown: false, headerTitle: "Scanner" }}
+        options={{
+          headerShown: true,
+          headerTitle: "",
+          headerRight: () => (
+            <Link href="/scan/all-items" asChild>
+              <Pressable>
+                <Feather name="list" size={24} color="black" />
+              </Pressable>
+            </Link>
+          ),
+        }}
       />
       <Stack.Screen name="[id]" options={{ headerTitle: "Scan Details" }} />
+      <Stack.Screen name="all-items" options={{ headerTitle: "All Items" }} />
     </Stack>
   );
 }
+
