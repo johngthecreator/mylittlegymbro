965c5d30856d5f423bf62c0fbf71cd10ff194e7c
John Gorriceta <johngorriceta1@gmail.com>
2025-09-09 01:57:55 -0600

    ios 26 compatible

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,56 +1,66 @@
-import { Tabs } from "expo-router";
+import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
 import React from "react";
-
-import { HapticTab } from "@/components/HapticTab";
-import { IconSymbol } from "@/components/ui/IconSymbol";
-import { Colors } from "@/constants/Colors";
 
 export default function TabLayout() {
   const colorScheme = "dark";
 
   return (
-    <Tabs
-      initialRouteName="index" // Set 'index' as the initial route for the Home tab
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarStyle: {
-          backgroundColor: "#3A3A3A",
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="scan"
-        options={{
-          title: "Scanner",
-          tabBarLabel: "Scanner",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="magnifyingglass" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarLabel: "Home",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
-          ),
-        }}
-      />
-      {/* Add your profile tab here */}
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-          tabBarLabel: "Profile",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="person.fill" color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    // <Tabs
+    //   initialRouteName="index" // Set 'index' as the initial route for the Home tab
+    //   screenOptions={{
+    //     tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+    //     headerShown: false,
+    //     tabBarButton: HapticTab,
+    //     tabBarStyle: {
+    //       backgroundColor: "#3A3A3A",
+    //     },
+    //   }}
+    // >
+    //   <Tabs.Screen
+    //     name="scan"
+    //     options={{
+    //       title: "Scanner",
+    //       tabBarLabel: "Scanner",
+    //       tabBarIcon: ({ color }) => (
+    //         <IconSymbol size={28} name="magnifyingglass" color={color} />
+    //       ),
+    //     }}
+    //   />
+    //   <Tabs.Screen
+    //     name="index"
+    //     options={{
+    //       title: "Home",
+    //       tabBarLabel: "Home",
+    //       tabBarIcon: ({ color }) => (
+    //         <IconSymbol size={28} name="house.fill" color={color} />
+    //       ),
+    //     }}
+    //   />
+    //   {/* Add your profile tab here */}
+    //   <Tabs.Screen
+    //     name="profile"
+    //     options={{
+    //       title: "Profile",
+    //       tabBarLabel: "Profile",
+    //       tabBarIcon: ({ color }) => (
+    //         <IconSymbol size={28} name="person.fill" color={color} />
+    //       ),
+    //     }}
+    //   />
+    // </Tabs>
+    <NativeTabs>
+      <NativeTabs.Trigger name="scan">
+        <Icon sf="magnifyingglass" drawable="custom_settings_drawable" />
+        <Label>Scanner</Label>
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="index">
+        <Label>Home</Label>
+        <Icon sf="house.fill" drawable="custom_android_drawable" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="profile">
+        <Icon sf="person.fill" drawable="custom_settings_drawable" />
+        <Label>Profile</Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
   );
 }
